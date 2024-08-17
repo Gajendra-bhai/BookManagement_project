4f84114bf2cb3fbc23ab84ed2b647a1260d17afc
Gajendra-bhai <87705336+Gajendra-bhai@users.noreply.github.com>
2024-08-17 14:06:48 +0530

    Add files via upload

---
+++ ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookManagement.DataAccess.Entity;
+
+namespace BookManagement.DataAccess.ServiceInterface
+{
+    public interface ICategoryRepository
+    {
+        List<CategoryEntity> GetAll();
+        bool Add(CategoryEntity entity);
+        bool Delete(int CategoryId);
+        void Update(CategoryEntity entity);
+
+    }
+}
